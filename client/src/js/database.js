5bbf968c3bdcdb8971ea64a547dfa4cc7f1e7637
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-23 19:00:18 -0700

    fixed get all methos

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,20 +18,18 @@ export const putDb = async (id, content) => {
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ jate: content });
   const result = await request;
   console.log("🚀 - data saved to the database", result);
-  // console.error("putDb not implemented");
 };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET all from the database");
   const jateDb = await openDB("jate", 1);
-  const tx = jateDb.transaction("jate", "readwrite");
+  const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
-  const request = store.add({ jate: content });
+  const request = store.getAll();
   const result = await request;
-  console.log("🚀 - data saved to the database", result);
-  // console.error("getDb not implemented");
+  console.log(result);
 };
 initdb();
